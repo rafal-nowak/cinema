26671805a286bb686329c481bf6516ab1d3ca307
rafal-nowak <contact@rafal-nowak.com>
2024-07-03 11:36:14 +0200

    initial commit

---
+++ src/main/java/com/rafalnowak/cinema/CinemaPocApplication.java
@@ -0,0 +1,13 @@
+package com.rafalnowak.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafalnowak/cinema/CinemaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafalnowak.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
