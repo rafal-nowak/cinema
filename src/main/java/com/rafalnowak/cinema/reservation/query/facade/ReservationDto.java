d8385517684ccb578d33204dde40fc9cf56a3313
rafal-nowak <contact@rafal-nowak.com>
2024-08-01 09:40:56 +0200

    refactor

--- src/main/java/com/rafalnowak/cinema/reservation/query/facade/ReservationDto.java
+++ src/main/java/com/rafalnowak/cinema/reservation/query/facade/ReservationDto.java
@@ -3,7 +3,6 @@ package com.rafalnowak.cinema.reservation.query.facade;
 import java.util.List;
 
 public record ReservationDto(
-        Integer id,
         String reservationNumber,
         List<SeatDto>seats
 ) {}
